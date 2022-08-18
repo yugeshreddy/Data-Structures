682805dca190fb2d3243e23ea92924fc8523a58c
Yugesh <yugesh265@gmail.com>
2022-08-18 06:54:43 +0530

    commit

---
+++ Stacks/Balanced Parenthesis.java
@@ -0,0 +1,40 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class GFG {
+
+	public static boolean matching(char a, char b) {
+		return ((a == '(' && b == ')') || (a == '[' && b == ']') || (a == '{' && b == '}'));
+	}
+
+	public static boolean isBalanced(String str) {
+
+		Deque<Character> s = new ArrayDeque<>();
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '(' || str.charAt(i) == '[' || str.charAt(i) == '{') {
+				s.add(str.charAt(i));
+			} else {
+				if (s.isEmpty() == true)
+					return false;
+				else if (matching(s.peek(), str.charAt(i)) == false)
+					return false;
+				else
+					s.pop();
+			}
+		}
+		return (s.isEmpty() == true);
+	}
+
+	public static void main(String[] args) {
+
+		String str = "{()}[]";
+
+		if (isBalanced(str))
+			System.out.print("Balanced");
+		else
+			System.out.print("Not Balanced");
+
+	}
+
+}
