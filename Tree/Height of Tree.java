6a2037a77677156cf776943ed799b1ab21672d69
Yugesh <yugesh265@gmail.com>
2022-08-22 10:27:50 +0530

    commit

---
+++ Tree/Height of Tree.java
@@ -0,0 +1,32 @@
+package demo;
+
+class Node {
+	int key;
+	Node left;
+	Node right;
+
+	Node(int k) {
+		key = k;
+		left = right = null;
+	}
+}
+
+public class test1 {
+
+	public static void main(String args[]) {
+		Node root = new Node(10);
+		root.left = new Node(20);
+		root.right = new Node(30);
+		root.right.left = new Node(40);
+		root.right.right = new Node(50);
+
+		height(root);
+	}
+
+	public static int height(Node root) {
+		if (root == null)
+			return 0;
+		else
+			return (1 + Math.max(height(root.left), height(root.right)));
+	}
+}
