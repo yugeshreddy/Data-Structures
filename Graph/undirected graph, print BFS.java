4ac2b686684812abb7a1df149e04b8b00facab3e
Yugesh <yugesh265@gmail.com>
2022-09-04 18:39:15 +0530

    commit

---
+++ Graph/undirected graph, print BFS.java
@@ -0,0 +1,57 @@
+package demo;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class test1 {
+
+	static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v) {
+		adj.get(u).add(v);
+		adj.get(v).add(u);
+	}
+
+	static void BFS(ArrayList<ArrayList<Integer>> adj, int V, int s) {
+		boolean[] visited = new boolean[V];
+		for (int i = 0; i < V; i++)
+			visited[i] = false;
+
+		Queue<Integer> q = new LinkedList<>();
+
+		visited[s] = true;
+		q.add(s);
+
+		while (q.isEmpty() == false) {
+			int u = q.poll();
+			System.out.print(u + " ");
+
+			for (int v : adj.get(u)) {
+				if (visited[v] == false) {
+					visited[v] = true;
+					q.add(v);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int V = 5;
+		ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(V);
+
+		for (int i = 0; i < V; i++)
+			adj.add(new ArrayList<Integer>());
+
+		addEdge(adj, 0, 1);
+		addEdge(adj, 0, 2);
+		addEdge(adj, 1, 2);
+		addEdge(adj, 2, 3);
+		addEdge(adj, 1, 3);
+		addEdge(adj, 3, 4);
+		addEdge(adj, 2, 4);
+
+		System.out.println("Following is Breadth First Traversal: ");
+		BFS(adj, V, 0);
+	}
+}
+output:
+Following is Breadth First Traversal: 
+0 1 2 3 4 
